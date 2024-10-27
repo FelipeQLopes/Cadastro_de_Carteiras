a8368c804ee35d44eb990703b25af11d23d4dd7b
Felipe Quites Lopes <quites@Felipes-MacBook-Air.local>
2024-10-27 17:54:02 -0300

    Script Preview-Box

--- public/script.js
+++ public/script.js
@@ -1,3 +1,21 @@
+const colorPreview = document.getElementById('preview-box');
+const iconPreview = document.getElementById('icon-preview');
+
+document.querySelectorAll('.color-picker input').forEach(input => {
+    input.addEventListener('change', (event) => {
+        const color = event.target.value;
+        colorPreview.style.backgroundColor = color;
+    });
+});
+
+document.querySelectorAll('.icon-picker input').forEach(input => {
+    input.addEventListener('change', (event) => {
+        const icon = event.target.value;
+        iconPreview.textContent = icon;
+    });
+});
+
+
 function validateForm() {
     const fields = ["nomeConta", "saldo", "descricaoConta", "meta", "detalhes"];
     for (const field of fields) {
